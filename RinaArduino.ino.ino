b470d4438b76ac97f6a36aeb23461200e0ae4cae
rinaandriyani <rinaandriyani38@gmail.com>
2016-09-26 10:19:32 +0700

    Add files via upload

---
+++ RinaArduino.ino.ino
@@ -0,0 +1,13 @@
+//the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output
+  pinMode (3, OUTPUT);
+}
+
+//the loop function runs over and over again forever
+void loop() {
+  digitalWrite(3, HIGH); //turn the LED on (HIGH is the voltage level)
+  delay(2500);            //wait for a scond
+  digitalWrite(3,LOW);  //turn the LED off by making the voltage low
+  delay(2500);            //wait for a scound
+}
